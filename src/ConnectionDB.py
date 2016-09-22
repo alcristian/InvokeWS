88f33b0a8abadf79e87fb2a592270676a6c1c353
Alvaro Cristian Botelho <cristian.ucpel@gmail.com>
2016-09-22 10:05:05 -0300

    review
    review

--- src/ConnectionDB.py
+++ src/ConnectionDB.py
@@ -10,15 +10,9 @@ class ConnectionDB():
             str = "INTEG_ODI_TMP"
         
         conns = { 
-                 "INTEG_FWK_HG":{"jdbc" : "jdbc:oracle:thin:@db-desa-sp.transacional.getnet.local:1521/integracaosrv",
-                                 "usr"  : "INTEG_FRAMEWORK_WR",
-                                 "pwd"  : "getnet123wr"},
-                 "INTEG_LOGS_HG":{"jdbc" : "jdbc:oracle:thin:@dsv12c-bd-hg-cb.getnet.local:1539/INTCREDHGSRV",
-                                 "usr"  : "INTEG_LOGS",
-                                 "pwd"  : "rvnmney3jhjh#2094"},
-                 "INTEG_ODI_TMP":{"jdbc" : "jdbc:oracle:thin:@db-desa-sp.transacional.getnet.local:1521/integracaosrv",
-                                 "usr"  : "INTEG_ODI_TMP",
-                                 "pwd"  : "INTEG_ODI_TMP"},
+                 "INTEG_FWK_HG":{"jdbc" : "xxx",
+                                 "usr"  : "xxx",
+                                 "pwd"  : "xxx"},
                  }
         
         jdbc = conns.get(str).get("jdbc")
